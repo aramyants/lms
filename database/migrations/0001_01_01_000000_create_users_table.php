c0aaef0cc5c5920fda0ef7734b4cf9ba248830e9
kamalyanaram <aramaramyants@gmail.com>
2025-03-04 19:59:26 +0400

    Initial commit

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -19,6 +19,9 @@ return new class extends Migration
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
+            $table->string('role')->default('student');
+            $table->string('timezone')->nullable();
+            $table->text('bio')->nullable();
         });
 
         Schema::create('password_reset_tokens', function (Blueprint $table) {
